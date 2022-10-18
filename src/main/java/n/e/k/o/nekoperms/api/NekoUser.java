1953111b4f8ae530cdcf4c3ad6ad888ac2c782c7
HoverCatz <1442391+HoverCatz@users.noreply.github.com>
2022-10-18 03:08:21 +0200

    Update NekoUser.java

--- src/main/java/n/e/k/o/nekoperms/api/NekoUser.java
+++ src/main/java/n/e/k/o/nekoperms/api/NekoUser.java
@@ -371,4 +371,14 @@ public class NekoUser
         return false;
     }
 
+    /**
+     * Used to determine whether the user making this call is subject to
+     * teleportations.
+     * @return whether the user making this call is a goat
+     */
+    public boolean isUserAGoat()
+    {
+        return false;
+    }
+
 }
